57d16934a9c2eb2b2d00b7d03d702a9ebf21a0aa
Pos-cmd <afolabiyasser06@gmail.com>
2025-01-05 19:25:11 +0000

    feat(admin): :sparkles: ajout d'un composant FieldForm avec gestion de l'affichage du tiroir

---
+++ admin/src/shared/drawers/FieldForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const showDrawer = ref(false)
+
+const handleOpenDrawer = () => {
+  showDrawer.value = true
+}
+
+defineExpose({
+  showDrawer,
+  handleOpenDrawer
+})
+</script>
+
+<template>
+   <n-drawer v-model:show="showDrawer" :width="502">
+    <n-drawer-content title="Stoner">
+      Stoner is a 1965 novel by the American writer John Williams.
+    </n-drawer-content>
+  </n-drawer>
+</template>
