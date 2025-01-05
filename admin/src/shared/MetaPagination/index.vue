e568ba68d9f1f0b41ec388bec89a8b8d2c9683c9
Pos-cmd <afolabiyasser06@gmail.com>
2025-01-05 15:04:34 +0000

    feat(admin): :sparkles: ajout du composant MetaPagination pour la gestion de la pagination

---
+++ admin/src/shared/MetaPagination/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import type { IBasicLazyPaginateMeta } from '@/services/api/type'
+
+  const props = withDefaults(defineProps<{
+    meta: IBasicLazyPaginateMeta<any>['meta']
+    size?: 'small' | 'medium' | 'large'
+    pageSlot?: number
+    changePage?: (page: number) => void
+  }>(), {
+    size: 'medium',
+    pageSlot: 6
+  })
+
+  const changePage = props.changePage
+</script>
+
+<template>
+  <n-pagination
+    class="flex-je"
+    :size
+    :page-slot
+    :page="meta?.currentPage"
+    :page-count="meta?.lastPage"
+    :per-page="meta?.perPage"
+    @update:page="changePage" />
+</template>
