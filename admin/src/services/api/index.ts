72191ba812479ff90471f0ac5b8ba636673756c4
Pos-cmd <afolabiyasser06@gmail.com>
2025-01-05 15:03:47 +0000

    feat(admin): :sparkles: ajout du contrôleur de gestion de contenu à l'API

--- admin/src/services/api/index.ts
+++ admin/src/services/api/index.ts
@@ -1,9 +1,11 @@
 import authController from './auth/index'
 import routeController from './route/index'
+import contentManagementController from './content_management'
 // import userController from './user'
 
 export const API = {
   // user: userController,
   auth: authController,
   route: routeController,
+  contentManagement: contentManagementController
 }
